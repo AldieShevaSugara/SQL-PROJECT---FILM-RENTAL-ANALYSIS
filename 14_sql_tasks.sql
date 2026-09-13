28bc64ee3565296fca3c7976134562df3e320907
AldieShevaSugara <aldiesheva111@gmail.com>
2026-09-13 11:11:24 +0700

    Update 14_sql_tasks.sql

--- 14_sql_tasks.sql
+++ 14_sql_tasks.sql
@@ -1,49 +1,49 @@
-SQL
+#TASK SQL
 
-1 All film with PG-13 film with rental rate of 2.99 or lower
+#1 All film with PG-13 film with rental rate of 2.99 or lower
 
 	SELECT title, rating, rental_rate 
    	FROM sakila.film f
 	WHERE rating = 'PG-13' 
    	AND rental_rate<=2.99
 
-2 All film that have deleted scenes
+#2 All film that have deleted scenes
 
   	SELECT title, special_features 	
   	FROM sakila.film f
   	WHERE special_features LIKE '%deleted scenes%'
 
-3. All active customers
+#3. All active customers
 
   	SELECT CONCAT(first_name,' ',last_name) 'Full name', active 
   	FROM sakila.customer
   	WHERE active = 1
 
-4. Names of customers who rented a movie on 26th July 2005
+#4. Names of customers who rented a movie on 26th July 2005
 
   	SELECT CONCAT(c.first_name,' ', c.last_name) 'Full name', DATE(r.rental_date) 
   	FROM sakila.rental r
   	JOIN sakila.customer c ON c.customer_id = r.customer_id
   	WHERE DATE(r.rental_date) = '2005-07-26';
 
-5. Distinct names of customers
+#5. Distinct names of customers
 
   	SELECT distinct concat(c.first_name,' ', c.last_name) 'Full name' 
   	FROM sakila.rental r
   	JOIN sakila.customer c ON c.customer_id = r.customer_id
   	WHERE DATE(r.rental_date) = '2005-07-26';
 
-6. How many distinct last names we have in the data
+#6. How many distinct last names we have in the data
 	
   	SELECT count(distinct c.last_name) 'Distinct customer last name' 
   	FROM sakila.customer c;
 
-7. How many rentals we do in each day?
+#7. How many rentals we do in each day?
 
   	SELECT DATE(rental_date), count(*) FROM sakila.rental r
   	GROUP BY DATE(rental_date);
 
-8 Busiest day?
+#8 Busiest day?
 
   	SELECT DATE(rental_date), count(*) AS SUM_RENTAL
   	FROM sakila.rental r
@@ -51,7 +51,7 @@ SQL
   	ORDER BY sum_rental DESC
   	LIMIT 1;
 
-9. All Sci-fi film in our catalogue
+#9. All Sci-fi film in our catalogue
 
   	SELECT fc.film_id, fc.category_id, c.name, f.title 
   	FROM sakila.film_category fc
@@ -59,14 +59,14 @@ SQL
   	JOIN sakila.film f ON f.film_id = fc.film_id
   	WHERE c.name='Sci-fi';
 
-10 Customers and how many movies they rented from us so far?
+#10 Customers and how many movies they rented from us so far?
 
   	SELECT c.first_name, r.customer_id, count(*) 
   	FROM sakila.rental r
   	JOIN sakila.customer c ON c.customer_id = r.customer_id
   	GROUP BY customer_id
 
-11 Which movies should we discontinue from our catalogue (<=1 lifetime rental)
+#11 Which movies should we discontinue from our catalogue (<=1 lifetime rental)
 
   	SELECT r.inventory_id, i.film_id, f.title, count(*) 
   	FROM sakila.rental r
@@ -89,7 +89,7 @@ SQL
   	JOIN sakila.inventory i ON i.inventory_id = ls.inventory_id
   	join sakila.film f on f.film_id = i.film_id
 
-12. Which movies are not returned yet?
+#12. Which movies are not returned yet?
 
   	SELECT f.title, r.return_date 
   	FROM sakila.rental r
@@ -97,7 +97,7 @@ SQL
   	JOIN sakila.film f ON f.film_id = i.film_id
   	WHERE r.return_date is null
 
-13 How much money and rentals we make for store 1 by day?
+#13 How much money and rentals we make for store 1 by day?
 
   	SELECT DATE(rental_date) AS DATE, SUM(p.amount) AS EARNING, COUNT(r.rental_id) AS TOTAL_RENT
   	FROM sakila.rental r
@@ -107,7 +107,7 @@ SQL
   	GROUP BY DATE(rental_date)
   	ORDER BY DATE(rental_date)
 
-14 What are top 3 earning days so far?
+#14 What are top 3 earning days so far?
 
   	SELECT DATE(rental_date) AS DATE, SUM(p.amount) AS EARNING, COUNT(r.rental_id) AS TOTAL_RENT 
     FROM sakila.rental r
